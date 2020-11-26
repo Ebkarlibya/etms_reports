e4fff941cff0d1ee00fb4540a539cdf432e436a9
Imad Abdou <igentle.appletec@gmail.com>
2020-11-26 13:39:57 +0100

    rename field (Website) to (Bank Website) and push it to the very end

--- etms_reports/etms_reports/report/bank_account_report/bank_account_report.py
+++ etms_reports/etms_reports/report/bank_account_report/bank_account_report.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 
+from frappe import _
 import frappe
 # from erpnext.accounts.doctype.payment_entry.payment_entry import get_account_details
 
@@ -18,7 +19,7 @@ def get_columns(filters):
     return [
 		{
 			"fieldname": "account_name",
-			"label": "Account Name",
+			"label": _("Account Name"),
 			"fieldtype": "data",
             "read_only": 1,
             "width": 150
@@ -26,45 +27,45 @@ def get_columns(filters):
 		},
 		{
 			"fieldname": "account",
-			"label": "Company Account",
+			"label": _("Company Account"),
 			"fieldtype": "Link",
 			"options": "Account",
             "width": 150
 		},
 		{
 			"fieldname": "bank",
-			"label": "Bank",
+			"label": _("Bank"),
 			"fieldtype": "Link",
 			"options": "Bank",
-            "width": 120
-		},
-		{
-			"fieldname": "website",
-			"label": "Website",
-			"fieldtype": "Data",
-			"read_only": 1,
-            "width": 120
+            "width": 150
 		},
         {
             "fieldname": 'account_currency',
-            "label": "Account Currency",
+            "label": _("Account Currency"),
             "fieldtype": "Read Only",
             "options": "Currency",
-            "width": 120
+            "width": 150
         },
         {
             "fieldname": 'account_balance',
-            "label": "Account Balance",
+            "label": _("Account Balance"),
             "fieldtype": "Read Only",
-            "width": 120
+            "width": 150
         },
         {
 			"fieldname": "company",
-			"label": "Company",
+			"label": _("Company"),
 			"fieldtype": "data",
             "read_only": 1,
-            "width": 120
+            "width": 150
 			
+		},
+        {
+			"fieldname": "website",
+			"label": _("Bank Website"),
+			"fieldtype": "Data",
+			"read_only": 1,
+            "width": 150
 		},
 	]
     
@@ -95,10 +96,10 @@ def get_data(filters):
         	'account_name': res[idx].account_name,
             'account': res[idx].account,
             'bank': res[idx].bank,
-            'website': res[idx].website,
             'account_currency': res[idx].account_currency,
             'account_balance': res[idx].account_balance,
             'company': res[idx].account,
+            'website': res[idx].website,
         }
         data.append(row)
         idx+=1
